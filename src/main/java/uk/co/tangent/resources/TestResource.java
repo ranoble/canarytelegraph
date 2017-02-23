846c5b66a7666351660f66e9003e5e095df26cad
Szabolcs Gyurko <szabolcs.gyurko@tangentsnowball.com>
2017-02-23 13:05:03 +0000

    Revert "Removing all @UnitOfWork annotations on resources, as the session is not required for the resources and no functions manipulate anything directly. The services take care of session handling themselves."
    This reverts commit 28c65dc3de3941e275012ca00266828a12e4ce67.

--- src/main/java/uk/co/tangent/resources/TestResource.java
+++ src/main/java/uk/co/tangent/resources/TestResource.java
@@ -1,5 +1,6 @@
 package uk.co.tangent.resources;
 
+import io.dropwizard.hibernate.UnitOfWork;
 import io.dropwizard.views.View;
 import uk.co.tangent.entities.Test;
 import uk.co.tangent.resources.view.TestView;
@@ -23,11 +24,13 @@ public class TestResource {
 
     @GET
     @Path("/{id}")
+    @UnitOfWork
     public Test getTest(@PathParam("id") Long id) {
         return service.getTest(id);
     }
 
     @POST
+    @UnitOfWork
     public Test insert(Test test) {
         test.setId(null);
         return service.save(test);
@@ -35,6 +38,7 @@ public class TestResource {
 
     @PUT
     @Path("/{id}")
+    @UnitOfWork
     public Test update(@PathParam("id") Long id, Test test) {
         test.setId(id);
         return service.save(test);
@@ -42,6 +46,7 @@ public class TestResource {
 
     @DELETE
     @Path("/{id}")
+    @UnitOfWork
     public Test update(@PathParam("id") Long id) {
         Test test = service.getTest(id);
         return service.delete(test);
@@ -54,6 +59,7 @@ public class TestResource {
     }
 
     @GET
+    @UnitOfWork
     public List<Test> list() {
         return service.getTests();
     }
